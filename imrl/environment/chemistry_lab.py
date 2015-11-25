e5ae0e2c81354f62b20857b1761ef27d80824be2
Xander Dunn <xander.dunn@icloud.com>
2015-11-25 12:20:57 -0800

    Chemistry lab mostly finished
    next_state() needs to be finished and the assumption that the reward
    radius of a subgoal is equal to the corridor radius is potentially
    incorrect.

--- imrl/environment/chemistry_lab.py
+++ imrl/environment/chemistry_lab.py
@@ -1,5 +1,8 @@
 """Chemistry lab environment."""
 
+# System
+import collections
+
 # Third party
 import numpy as np
 
@@ -7,9 +10,13 @@ import numpy as np
 from imrl.environment.environment import Environment
 from imrl.agent.option.option import Subgoal
 
-# Subgoals are evenly spaced points with the same radius, not necessarily the width of the cooridor.
-# Corridors between these points have a width outside of which the agent is sent back to the start state.
-# Might represent it as a graph.  Edges are corridors between them.
+
+class SubgoalNode(Subgoal):
+    """A subgoal that contains a list connecting subgoals.  Set root = True if this is the root of the graph."""
+    def __init__(self, state, radius=0.1):
+        super(SubgoalNode, self).__init__(state, radius)
+        self.connections = []
+        self.root = False
 
 
 class ChemistryLab(Environment):
@@ -24,7 +31,7 @@ class ChemistryLab(Environment):
         self.reward_radius = reward_radius
 
     def reward_vector(self):
-        # TODO
+        # TODO: This will explode when called
         return self.num_states() - 1
 
     def reward(self, state):
@@ -33,25 +40,79 @@ class ChemistryLab(Environment):
     def initial_state(self):
         """The starting position is 0 at every dimension except the first, which starts at 0.5."""
         position = [max(np.random.normal(0, self.move_sd), 0.0) for _ in range(self.num_actions)]
-        position = position[:, 0] = 0.5
+        position[0] = 0.5
         return position
 
-    def create_subgoals(self):
-        # TODO
-        subgoal_states = list(islice(range(1, self.num_states() + 1), 0, None, self.l_tumbler_length))
-        return list(map(Subgoal, subgoal_states))
+    @staticmethod
+    def create_subgoals():
+        """Create connected subgoals as described on the whiteboard and return the root node."""
+        a = SubgoalNode([0.5, 0.25])
+        a.root = True
+        b = SubgoalNode([0.25, 0.25])
+        c = SubgoalNode([0.75, 0.25])
 
-    def is_terminal(self, state):
-        """Return true if the agent has made a wrong move."""
-        # TODO: Test that the potential position is within at least one edge to a conencted Subgoal
-        # TODO: Test if the state is at the final subgoal
-        is_terminal = False
-        for x, y in zip(actions, self.solution):
-            if x != y:
-                is_terminal = True
+        d = SubgoalNode([0.25, 0.5])
+        e = SubgoalNode([0.5, 0.5])
+        f = SubgoalNode([0.75, 0.5])
+
+        g = SubgoalNode([0.25, 0.75])
+        h = SubgoalNode([0.5, 0.75])
+        i = SubgoalNode([0.75, 0.75])
+
+        a.connections += [b, c]
+        b.connections.append(d)
+        c.connections.append(f)
+
+        d.connections.append(e)
+        e.connections.append(h)
+
+        h.connections += [g, i]
+        return a
+
+
+    @staticmethod
+    def is_within_subgoals(state, subgoal1, subgoal2):
+        """Given two subgoals, check if the agent's state is between them.  The subgoals are SubgoalNode objects and the state is an n-dimensional list of agent's position values."""
+        within = True
+        epsilon = subgoal1.radius
+        for i in range(len(state)):  # Iterate all the state dimensions
+            print(i)
+            sorted_values = sorted([subgoal1.state[i], subgoal2.state[i]])
+            print(sorted_values)
+            print(epsilon)
+            within = state[i] <= sorted_values[1] + epsilon and state[i] >= sorted_values[0] - epsilon
+            print(state[i])
+            if not within:
                 break
-        if actions[:-1] == self.solution:
-            is_terminal = True
+        print(within)
+        print('\n')
+        return within
+
+    def is_terminal(self, state, subgoal):
+        """Return true if the agent made a wrong move.  Given a subgoal (usually the root subgoal), check if the agent is within some valid connection between subgoals."""
+        is_terminal = True
+        next_subgoals = collections.deque([subgoal])
+        while (len(next_subgoals) > 0):
+            print(len(next_subgoals))
+            current_subgoal = next_subgoals.pop()
+            for next_subgoal in current_subgoal.connections:
+                print('Adding subgoals {}'.format(next_subgoal))
+                next_subgoals.appendleft(next_subgoal)
+            for connection in current_subgoal.connections:
+                is_terminal = not ChemistryLab.is_within_subgoals(state, current_subgoal, connection)
+                if not is_terminal:
+                    print('Got is_terminal=False from subgoal {} and connection {} and state {}'.format(subgoal, connection, state))
+                    break
+            if not is_terminal:
+                break
+        print('is_terminal = {}'.format(is_terminal))
+        print('subgoal.root = {}'.format(subgoal.root))
+        if is_terminal and subgoal.root:
+            # Create a fake initial state subgoal just to see if it's in the corridor between initial state and the root subgoal
+            fake_initial_state_subgoal = SubgoalNode(self.initial_state())
+            is_terminal = not ChemistryLab.is_within_subgoals(state, subgoal, fake_initial_state_subgoal)
+            if not is_terminal:
+                print('Got is_terminal=False from subgoal {} and connection {} and state {}'.format(subgoal, fake_initial_state_subgoal, state))
         return is_terminal
 
     def next_state(self, state, action):
--- tests/test_chemistry_lab.py
+++ tests/test_chemistry_lab.py
@@ -1,8 +1,43 @@
 """Test the chemistry lab environment."""
 
-# System
-import random
-
 # First party
-from imrl.environment.chemistry_lab import ChemistryLab
-from imrl.agent.option.option import Subgoal
+from imrl.environment.chemistry_lab import ChemistryLab, SubgoalNode
+
+
+def test_within_subgoals():
+    """Test that it correctly identifies a state between or not between two subgoals."""
+    a = SubgoalNode([0.5, 0.25])
+    b = SubgoalNode([0.25, 0.25])
+    c = SubgoalNode([0.5, 0.75])
+    a.connections += [b, c]
+    print(a.connections)
+
+    assert not ChemistryLab.is_within_subgoals([0.5, 0.5], a, b)
+    assert ChemistryLab.is_within_subgoals([0.3, 0.25], a, b)
+    assert ChemistryLab.is_within_subgoals([0.2, 0.25], a, b)
+    assert not ChemistryLab.is_within_subgoals([0.145, 0.25], a, b)
+    assert ChemistryLab.is_within_subgoals([0.6, 0.6], a, c)
+
+
+def test_subgoals():
+    """Test that the created subgoals are correct."""
+    root_subgoal = ChemistryLab.create_subgoals()
+    assert len(root_subgoal.connections) == 2
+    assert root_subgoal.root
+
+
+def test_is_terminal():
+    """Test that the state goes terminal when the agent makes a move outside the corridors."""
+    lab = ChemistryLab()
+    root_subgoal = ChemistryLab.create_subgoals()
+
+    assert not lab.is_terminal(lab.initial_state(), root_subgoal)
+    assert not lab.is_terminal([0.55, 0.4], root_subgoal)
+    assert not lab.is_terminal([0.25, 0.4], root_subgoal)
+    assert lab.is_terminal([0, 0], root_subgoal)
+    assert lab.is_terminal([0.25, 0.64], root_subgoal)
+    assert lab.is_terminal([1, 1], root_subgoal)
+    assert not lab.is_terminal([0.64, 0.75], root_subgoal)
+    assert not lab.is_terminal([0.64, 0.70], root_subgoal)
+    assert not lab.is_terminal([0.25, 0.70], root_subgoal)
+    assert lab.is_terminal([0.64, 0.5], root_subgoal)
